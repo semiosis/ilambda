ceb43b3135508bdb98c24e7ecde599b438c8f4d6
Shane Mulligan <mullikine@gmail.com>
2021-10-06 02:39:53 +1300

    Wed 06 Oct 2021 02:39:53

---
+++ ilambda.hs
@@ -0,0 +1,16 @@
+-- A module for UNIX pipe utilities
+{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+module UnixFuncs where
+
+import System.Process
+
+-- Get the date with the UNIX date command
+getDate :: IO String
+getDate = readProcess "date" [] ""
+
+-- How do I use getDate in this main function?
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+  putStrLn getDate
