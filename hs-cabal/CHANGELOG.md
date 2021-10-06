328ac1d7aad1d3cdce5d114500be5eaec1a6e930
Shane Mulligan <mullikine@gmail.com>
2021-10-07 00:28:12 +1300

    07.10.21

--- hs-cabal/CHANGELOG.md
+++ hs-cabal/CHANGELOG.md
@@ -1,4 +1,4 @@
-# Revision history for ilambda-hs
+# Revision history for hs-cabal
 
 ## 0.1.0.0 -- YYYY-mm-dd
 
--- hs-stack/src/Lib.hs
+++ hs-stack/src/Lib.hs
@@ -7,7 +7,7 @@ import System.Process
 -- This works directly in ghci
 -- pickUpLine "Library"
 pickUpLine :: String -> IO String
-pickUpLine = readProcess "myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
+pickUpLine = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
 
 -- This would be fine for main = someFunc
 -- someFunc :: IO ()
