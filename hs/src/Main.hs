765c3db9c0eaf8c19312da6abd3b5a474bae12d8
Shane Mulligan <mullikine@gmail.com>
2021-10-11 11:00:21 +1300

    Mon 11 Oct 2021 11:00:21

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -3,10 +3,10 @@
 
 module Main where
 
+import System.Process ( readProcess )
 import Data.Aeson ( decode )
--- import Data.Aeson.Pretty as AesonPretty
-import qualified Data.ByteString.Lazy as BLU
-import GHC.Generics ()
+import qualified Data.ByteString.Lazy.UTF8 as BLU
+import Data.Maybe ( fromJust, fromMaybe )
 
 pickUpLine :: String -> IO String
 pickUpLine = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
@@ -14,10 +14,28 @@ pickUpLine = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-
 decodeResultsList :: String -> Maybe [String]
 decodeResultsList results = Data.Aeson.decode (BLU.fromString (Prelude.take (Prelude.length results - 1) results :: String)) :: Maybe [String]
 
-getResults :: String -> IO [String]
+getResults :: String -> IO (Maybe [String])
 getResults product = do
   results <- pickUpLine product
   return (decodeResultsList results)
 
 main :: IO ()
-main = print getResults "steel"
+main = putStrLn "hello world"
+
+-- main :: IO ()
+-- main = do
+--   linesJson <- pickUpLine "Haskell"
+--   -- decode linesJson using getResults
+
+
+-- main :: IO ()
+-- main = do
+--   putStrLn ((fromJust (getResults "Weather")) <> "\\n\\n\\n" <> (fromJust (getResults "Sunshine")))
+
+-- main = do
+--   linesJson <- pickUpLine "Haskell"
+--   lines <- decodeResultsList linesJson
+--   return $ map (Just . fromJust) lines
+
+--   -- -- print the Maybe [String], lines:
+--   -- mapM_ putStrLn (Just lines)
