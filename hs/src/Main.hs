f30d98858ce6a337cff437af39ce214b167f8174
Shane Mulligan <mullikine@gmail.com>
2021-10-11 21:16:16 +1300

    Mon 11 Oct 2021 21:16:16

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -7,6 +7,9 @@ import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Data.Maybe ( fromJust, fromMaybe )
 import Control.Monad ( forM_ )
 
+listOf :: String -> IO String
+listOf = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
+
 pickUpLine :: String -> IO String
 pickUpLine = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
 
