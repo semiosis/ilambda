1a5b40dd12e5ea8edff4ca6ae8783fac5aacebba
Shane Mulligan <mullikine@gmail.com>
2021-10-11 14:23:51 +1300

    Mon 11 Oct 2021 14:23:51

--- hs/src/Main.hs
+++ hs/src/Main.hs
@@ -1,12 +1,11 @@
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main where
-
 import System.Process ( readProcess )
 import Data.Aeson ( decode )
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Data.Maybe ( fromJust, fromMaybe )
+import Control.Monad ( forM_ )
 
 pickUpLine :: String -> IO String
 pickUpLine = readProcess "/home/shane/scripts/myeval" ["pena", "very-witty-pick-up-lines-for-a-topic/1"]
@@ -16,10 +15,10 @@ decodeResultsList results = Data.Aeson.decode (BLU.fromString (Prelude.take (Pre
 
 getResults :: String -> IO (Maybe [String])
 getResults product = do
-  results <- pickUpLine product
-  return (decodeResultsList results)
+    results <- pickUpLine product
+    return (decodeResultsList results)
 
 main :: IO ()
 main = do
-    result <- getResults "Weather"
-    print $ unlines $ fromMaybe [] result
+  output <- getResults "Weather"
+  print $ unlines $ fromMaybe [] output
