1c0bfff2ee22054920868f3a0854b05b3a5cc5ee
Shane Mulligan <mullikine@gmail.com>
2021-12-01 22:19:55 +1300

    Wed 01 Dec 2021 22:19:55

--- common-lisp/src/main.lisp
+++ common-lisp/src/main.lisp
@@ -7,5 +7,7 @@
   (format t "Hello, world.~%"))
 
 ;; Annoyingly, Codex doesn't do a great job at generating Common Lisp
+;; I have to concentrate on languages which work well with Codex.
+;; Though, perhaps this is an opportunity to build ilambda for Common Lisp.
 
 (main)
