0dad4b1e1a34d353028aaa736f7a20cc8140068d
Shane Mulligan <mullikine@gmail.com>
2021-12-01 22:42:53 +1300

    Wed 01 Dec 2021 22:42:53

--- clojure/src/ilambda/core.clj
+++ clojure/src/ilambda/core.clj
@@ -1,5 +1,11 @@
 (ns ilambda.core)
 
+;; TODO First, implement pen client
+;; /home/shane/var/smulliga/source/git/semiosis/pen.el/src/pen-client.el
+
+;; TODO Second, implement ilambda thin client
+;; /home/shane/var/smulliga/source/git/semiosis/pen.el/src/ilambda.el
+
 (defn foo "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
